164ce2bb2b3a02e58c8b125a5848acc90e1bc382
horuyama <horuyama0114@gmail.com>
2025-09-08 18:52:28 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,18 @@
-// v2: cache name bump to refresh assets
-const CACHE_NAME = "pay-opt-v2";
+// v5 cache bump
+const CACHE_NAME = "pay-opt-v5";
 self.addEventListener("install", e => {
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache =>
       cache.addAll([
-        "./index.html",
-        "./manifest.json",
-        "./pay_opt_rules.csv",
-        "./pay_opt_campaigns.csv",
-        "./icon-192.png",
-        "./icon-512.png"
+        "./index.html","./manifest.json","./pay_opt_rules.csv","./pay_opt_campaigns.csv",
+        "./icon-192.png","./icon-512.png"
       ])
     )
   );
 });
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
-    ))
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
   );
 });
 self.addEventListener("fetch", e => {
